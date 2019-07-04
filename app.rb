e130c396ff02f789f8ffdc426d76d82de2f61d94
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-04 16:39:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
     @number1 = @number1.to_i
     @number2 =  params[:number2]
     @number2 = @number2.to_i
-    <%=@number1*@number2%>
+    "#{@number1*@number2}"
   end
 
 end
